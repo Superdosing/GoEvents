2ec979339d24b8bc20e414e5bd7ff1d695406132
Danni Rosing <rosingdanni@gmail.com>
2017-02-20 11:48:42 +0100

    Html sider added, menuen skrevet ud(ingen styling) og de sidste mangler

---
+++ GoEvents/www/js/Main.js
@@ -0,0 +1,16 @@
+﻿
+$(function () {
+
+
+    $myDiv = $("#myDiv");
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:13105/api/Get/GetMenu',
+        success: function (posts) {
+            $.each(posts, function (i, post) {
+                $myDiv.append("<li><a href=\""+ post.Navn + ".html"+"\".>" + post.Navn + " </a></li>");
+            });
+        }
+    });
+
+});

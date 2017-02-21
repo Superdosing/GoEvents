77bedac6b5322ccb1d2ea9f974442caa643aa0e2
Danni Rosing <rosingdanni@gmail.com>
2017-02-21 12:37:48 +0100

    Lortet virker igen

--- GoEvents/www/js/my-app.js
+++ GoEvents/www/js/my-app.js
@@ -9,8 +9,12 @@ var $$ = Dom7;
 var mainView = myApp.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true
-})
-var Nyheder = myApp.addView('.another-view');
+});
+var view1 = myApp.addView('#view1');
+var view2 = myApp.addView('#view2');
+var view3 = myApp.addView('#view3');
+var view3 = myApp.addView('#view4');
+var anotherview = myApp.addView('.another-view');
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
@@ -21,7 +25,7 @@ $$(document).on('deviceready', function() {
 // Now we need to run the code that will be executed only for About page.
 
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function(page) {
+myApp.onPageInit('Events', function(page) {
     // Do something here for "about" page
 
 });
@@ -32,20 +36,13 @@ $$(document).on('pageInit', function(e) {
     // Get page data from event data
     var page = e.detail.page;
 
-    if (page.name === 'about') {
+    if (page.name === 'Events') {
         // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert();
+        
     }
 });
-$$(document).on('pageInit', function (e) {
-    // Get page data from event data
-    var page = e.detail.page;
 
-    if (page.name === 'Nyheder') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert();
-    }
-});
+
 
 
 
